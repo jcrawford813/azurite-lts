99d41a6339c9eca3103b8f4e4f4d0faf33d1cc10
Jim Crawford <jim@mycodinglife.com>
2025-06-05 12:19:22 -0500

    Initial checkin to set up workflow and test image build.

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,21 +4,31 @@ set -ouex pipefail
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+#Install Plasma
+dnf --enablerepo=epel,crb group -y install "KDE Plasma Workspaces"
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+#Install Flatpak
+dnf install flatpak -y
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+#Install other tooling
+dnf install distrobox \
+        rsms-inter-fonts \
+        libvirt-daemon-config-network \
+        libvirt-daemon-kvm \
+        swtpm-selinux \
+        fish \
+        qemu-kvm \ 
+        virt-manager \
+        ksshaskpass \
+        vorta \
+        borgbackup \
+        logiops \
+        solaar \
+        plasma-firewall-firewalld \
+        flatpak-kcm
 
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl disable gdm
+systemctl enable sddm
