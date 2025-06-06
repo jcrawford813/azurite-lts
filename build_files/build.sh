bdff51129247136015fcd588344a2cc2cfdf1a7f
Jim Crawford <jim@mycodinglife.com>
2025-06-05 20:00:21 -0500

    Fix copy script

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,8 +20,8 @@ dnf group -y install "KDE Plasma Workspaces" --allowerasing --nobest
 dnf install distrobox libvirt-daemon-config-network libvirt-daemon-kvm swtpm-selinux qemu-kvm virt-manager -y --allowerasing
 dnf install fish borgbackup plasma-firewall-firewalld -y --allowerasing
 
-cp -r etc /etc
-cp -r usr /usr
+cp -r etc/* /etc
+cp -r usr/* /usr
 
 systemctl enable podman.socket
 systemctl disable gdm
