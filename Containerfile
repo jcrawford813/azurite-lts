e5995e93ddffc000c775bae5dbe08e9f4168c18a
Jim Crawford <jim@mycodinglife.com>
2025-06-05 20:10:44 -0500

    Fix build script paths once again.

--- Containerfile
+++ Containerfile
@@ -23,6 +23,8 @@ RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/log \
     --mount=type=tmpfs,dst=/tmp \
     /ctx/build.sh && \
+    cp -r /ctx/etc/* /etc && \
+    cp -r /ctx/usr/* /usr && \
     ostree container commit
     
 ### LINTING
--- build_files/build.sh
+++ build_files/build.sh
@@ -20,9 +20,6 @@ dnf group -y install "KDE Plasma Workspaces" --allowerasing --nobest
 dnf install distrobox libvirt-daemon-config-network libvirt-daemon-kvm swtpm-selinux qemu-kvm virt-manager -y --allowerasing
 dnf install fish borgbackup plasma-firewall-firewalld -y --allowerasing
 
-cp -r etc/* /etc
-cp -r usr/* /usr
-
 systemctl enable podman.socket
 systemctl disable gdm
 systemctl enable sddm
