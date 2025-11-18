ba3306b45d0328a73bd7dc78a7f7b0591c923cf4
Jim Crawford <jim@mycodinglife.com>
2025-11-18 16:14:58 -0600

    Add cache cleanup to build script.

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,11 +13,10 @@ dnf update -y
 
 dnf group -y install "KDE Plasma Workspaces" --allowerasing --nobest
 
-#Enable COPR for cryfs
-dnf copr enable fcsm/cryfs -y
+dnf clean all
 
 #Install other tooling
-dnf install distrobox libvirt-daemon-config-network libvirt-daemon-kvm swtpm-selinux qemu-kvm virt-manager cryfs -y --allowerasing
+dnf install distrobox libvirt-daemon-config-network libvirt-daemon-kvm swtpm-selinux qemu-kvm virt-manager -y --allowerasing
 dnf install fish borgbackup plasma-firewall-firewalld -y --allowerasing
 
 systemctl enable podman.socket
